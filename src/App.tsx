4bf81edaf3756bd87e0c0e96de4f757246f3b9ac
assiscs2 <50466377+assiscs2@users.noreply.github.com>
2022-07-11 20:54:11 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from './router';
 
 
-export function App() {
+function App() {
   return (
       <BrowserRouter>
         <Router />
       </BrowserRouter>   
   )
 }
+
+export default App
 
 

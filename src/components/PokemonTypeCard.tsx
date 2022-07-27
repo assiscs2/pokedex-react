d5a8eb8316f6b33689bc0e7ae37ee7d0fb21081c
assiscs2 <50466377+assiscs2@users.noreply.github.com>
2022-07-27 18:01:02 -0300

    minor changes to the pokemon type colors

--- src/components/PokemonTypeCard.tsx
+++ src/components/PokemonTypeCard.tsx
@@ -6,7 +6,7 @@ export interface PokemonTypeProps {
 export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
   if (PokeTypeProps.typeName === "poison") {
     return (
-      <span className="bg-fuchsia-500 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#A040A0] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -14,7 +14,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "grass") {
     return (
-      <span className="bg-green-500 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#78C850] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -22,7 +22,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "fire") {
     return (
-      <span className="bg-orange-600 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#F08030] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -30,7 +30,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "flying") {
     return (
-      <span className="bg-blue-300 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-90 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#A890F0] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -38,7 +38,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "water") {
     return (
-      <span className="bg-blue-500 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#6890F0] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -46,7 +46,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "bug") {
     return (
-      <span className="bg-stone-500 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#A8B820] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -54,7 +54,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "normal") {
     return (
-      <span className="bg-slate-500 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#A8A878] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -62,7 +62,7 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "ground") {
     return (
-      <span className="bg-yellow-800 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#E0C068] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
@@ -70,14 +70,102 @@ export function PokemonTypeCard(PokeTypeProps: PokemonTypeProps) {
 
   if (PokeTypeProps.typeName === "electric") {
     return (
-      <span className="bg-yellow-400 border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+      <span className="bg-[#F8D030] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "rock") {
+    return (
+      <span className="bg-[#B8A038] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "unknown") {
+    return (
+      <span className="bg-[#68A090] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "steel") {
+    return (
+      <span className="bg-[#B8B8D0] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "psychic") {
+    return (
+      <span className="bg-[#F85888] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "ghost") {
+    return (
+      <span className="bg-[#705898] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "ice") {
+    return (
+      <span className="bg-[#98D8D8] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "arceus") {
+    return (
+      <span className="bg-[#B8A038] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "dark") {
+    return (
+      <span className="bg-[#705848] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "dragon") {
+    return (
+      <span className="bg-[#7038F8] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "fairy") {
+    return (
+      <span className="bg-[#EE99AC] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
+        {PokeTypeProps.typeName}
+      </span>
+    );
+  }
+
+  if (PokeTypeProps.typeName === "fighting") {
+    return (
+      <span className="bg-[#C03028] border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
         {PokeTypeProps.typeName}
       </span>
     );
   }
 
   return (
-    <span className="bg-yellow-800  border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center bg-opacity-80 hover:bg-opacity-100 transition-opacity text-gray-900">
+    <span className="bg-yellow-800  border-2 border-gray-900 rounded-lg px-2 py-1 font-semibold flex items-center justify-center text-gray-900">
       {PokeTypeProps.typeName}
     </span>
   );

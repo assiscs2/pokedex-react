b3e84b1363bbbf11177990f474b35760712ffa71
assiscs2 <50466377+assiscs2@users.noreply.github.com>
2022-07-27 16:19:01 -0300

    minor change to the page browsing feature

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -131,6 +131,7 @@ export function Home() {
       <div className="flex items-center justify-center pt-4 gap-8 text-xs">
         <button
           disabled={counter === 0 || loading}
+          className='disabled:opacity-40'
           onClick={() => {
             setCounter((queryLimit = counter - 12));
             setPageCounter(pageCounter - 1);
@@ -151,6 +152,7 @@ export function Home() {
         </span>
         <button
           disabled={loading}
+          className='disabled:opacity-40'
           onClick={() => {
             setCounter((queryLimit = counter + 12));
             setPageCounter(pageCounter + 1);

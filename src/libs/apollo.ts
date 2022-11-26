5321ed433ed531e9ce43e7f33484ff5da9bd1849
Gustavo Assis <assiscs2@gmail.com>
2022-11-26 17:07:26 -0300

    adding a proxy to fix CORS error

--- src/libs/apollo.ts
+++ src/libs/apollo.ts
@@ -3,7 +3,11 @@ import {
     InMemoryCache, 
   } from '@apollo/client';
 
+  // import { expressMiddleware } from '@apollo/server/express4';
+  // import cors from 'cors';
+
+
   export const client = new ApolloClient({
-    uri:'https://beta.pokeapi.co/graphql/v1beta',
+    uri:'https://mighty-fjord-76016.herokuapp.com/https://beta.pokeapi.co/graphql/v1beta',
     cache: new InMemoryCache(),
   })
